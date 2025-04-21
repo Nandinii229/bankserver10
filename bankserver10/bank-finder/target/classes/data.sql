dff5d3c83d025a606a5cf3e5c1d5481b28165ffb
Nandini kodali <nandinichowdary229@gmail.com>
2025-04-21 01:40:41 -0500

    version3

---
+++ bankserver10/bank-finder/target/classes/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO bank (name, address, city, state, zipcode, latitude, longitude, phone, hours) VALUES
+('Chase Bank', '123 Main St', 'New York', 'NY', '10001', 40.7128, -74.0060, '212-555-1234', '9:00 AM - 5:00 PM'),
+('Bank of America', '456 Broadway', 'New York', 'NY', '10001', 40.7129, -74.0061, '212-555-5678', '9:00 AM - 5:00 PM'),
+('Wells Fargo', '789 5th Ave', 'New York', 'NY', '10001', 40.7130, -74.0062, '212-555-9012', '9:00 AM - 5:00 PM'),
+('Citibank', '321 Park Ave', 'New York', 'NY', '10001', 40.7131, -74.0063, '212-555-3456', '9:00 AM - 5:00 PM'),
+('TD Bank', '654 Lexington Ave', 'New York', 'NY', '10001', 40.7132, -74.0064, '212-555-7890', '9:00 AM - 5:00 PM'); 
